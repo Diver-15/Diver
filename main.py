d3909cb7c3ab337a901c467f194176b2efa9a52d
hmhmchm <hyeonminchoi5@gmail.com>
2022-03-07 23:44:06 +0900

    feat : 로그인 회원가입 css처리

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print('Hello sparta!')
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+
+import config
+
+app = Flask(__name__)
+app.secret_key = 'chm'
+
+client = MongoClient(config.Mongo_key)
+db = client.dbsparta
+
+@app.route('/')
+def home():
+    return render_template('signin.html')
+
+@app.route("/signup")
+def signup_form():
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=8000, debug=True)
