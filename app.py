734f0f526bf8dbc2fb319ec41cbf59dbfc929808
hmhmchm <hyeonminchoi5@gmail.com>
2022-03-10 00:22:39 +0900

    feat : 좋아요 기능 추가

--- app.py
+++ app.py
@@ -7,11 +7,13 @@ import config
 import jwt
 import datetime
 import hashlib
+
 # client = MongoClient('mongodb://3.34.44.93', 27017, username="sparta", password="woowa")
 client = MongoClient(config.Mongo_key)
 db = client.dbsparta
 
 SECRET_KEY = 'SPARTA'
+
 
 #################################
 ##  HTML을 주는 부분             ##
@@ -39,6 +41,7 @@ def login():
 def register():
     return render_template('register.html')
 
+
 @app.route('/shopping')
 def shoppingmall():
     return render_template('shop.html')
@@ -60,12 +63,15 @@ def api_register():
     db.user.insert_one({'id': id_receive, 'pw': pw_hash, 'name': name_receive})
 
     return jsonify({'result': 'success'})
+
+
 # [회원가입 아이디 중복확인 API]
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
     username_receive = request.form['username_give']
     exists = bool(db.user.find_one({"id": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
+
 
 # [로그인 API]
 @app.route('/api/login', methods=['POST'])
@@ -105,16 +111,50 @@ def api_valid():
     except jwt.exceptions.DecodeError:
         return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
 
+
 # [쇼핑몰 관련 API]
 @app.route("/clothes_male", methods=["GET"])
 def male_get():
-    clothe_list = list(db.shoppingmall.find({'gender':'남'}, {'_id': False},))
+    clothe_list = list(db.shoppingmall.find({'gender': '남'}, {'_id': False}, ))
     return jsonify({'clothes': clothe_list})
+
 
 @app.route("/clothes_female", methods=["GET"])
 def female_get():
-    clothe_list = list(db.shoppingmall.find({'gender':'여'}, {'_id': False}))
+    token_reveive = request.cookies.get('mytoken')
+    payload = jwt.decode(token_reveive, SECRET_KEY, algorithms=['HS256'])
+
+    clothe_list = list(db.shoppingmall.find({'gender': '여'}))
+    for cloth in clothe_list:
+        cloth["_id"] = str(cloth["_id"])
+        cloth["count_heart"] = db.likes.count_documents({"post_id": cloth["_id"], "type": "heart"})
+        cloth["heart_by_me"] = bool(db.likes.find_one({"post_id": cloth["_id"], "type": "heart", "id": payload['id']}))
     return jsonify({'clothes': clothe_list})
+
+
+# 좋아요 API
+@app.route('/update_like', methods=['POST'])
+def update_like():
+    token_reveive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_reveive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.user.find_one({"id": payload["id"]})
+        post_id_receive = request.form["post_id_give"]
+        type_receive = request.form["type_give"]
+        action_receive = request.form["action_give"]
+        doc = {
+            "post_id": post_id_receive,
+            "username": user_info["id"],
+            "type": type_receive
+        }
+        if action_receive == "like":
+            db.likes.insert_one(doc)
+        else:
+            db.likes.delete_one(doc)
+        count = db.likes.count_documents({"post_id": post_id_receive, "type": type_receive})
+        return jsonify({"result": "success", 'msg': 'updated', "count": count})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=8000, debug=True)
